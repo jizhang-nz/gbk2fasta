62d8991c29222296dd02d51c43a6ab7498fc73a2
張集 (Ji Zhang) <32469668+jizhang-nz@users.noreply.github.com>
2019-02-07 14:55:00 +1300

    Update gbk2fasta.pl

--- gbk2fasta.pl
+++ gbk2fasta.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-#function: convert GenBank sequence file to FASTA format sequence file.
+#function: converting GenBank file into FASTA file.
 
 #Copy (C) 2018-2019 Massey University. All rights reserved
 #Written by Ji Zhang, MD, PhD
@@ -8,7 +8,7 @@
 
 use Bio::SeqIO;
 
-my $usage = "Example: perl gbk2fasta.pl in.gbk out.fasta\n"; 
+my $usage = "Example: perl gbk2fasta.pl in.gbk out.fas\n"; 
 my $infile = shift or die $usage; 
 my $outfile = shift or die $usage;
 
